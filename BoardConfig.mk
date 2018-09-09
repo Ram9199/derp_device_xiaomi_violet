50d7cace9c80ddfe87a28bb5e271949cb4096edd
Ananth Raghavan Subramanian <sananth@codeaurora.org>
2018-09-08 19:47:41 -0700

    violet: power: Introducing PowerHAL binderized service
    Moving from the default implementation to a binderized service with no
    wrapping.
    Now using 1.2 implementation of the IPower interface.
    
    Change-Id: Ibefd916544f51545e6889684a051b2ecc5867fe2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,8 +155,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USES_MKE2FS := true
 
 # Power
-TARGET_HAS_NO_POWER_STATS := true
-TARGET_HAS_NO_WLAN_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
 
 # QCOM
